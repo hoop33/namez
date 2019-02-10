cb4dbd920840b4c9f71cbc33c8ed5d421da2ed4b
Rob Warner <rwarner@grailbox.com>
2019-02-10 15:01:55 -0500

    Set up client to get names from server

--- packages/namez-client/src/index.js
+++ packages/namez-client/src/index.js
@@ -3,6 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {ApolloClient} from 'apollo-client';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {HttpLink} from 'apollo-link-http';
+import gql from 'graphql-tag';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:4000/',
+});
+const client = new ApolloClient({
+  cache,
+  link,
+});
+
+client
+  .query({
+    query: gql`
+      query GetNames {
+        names {
+          cursor
+          nodes {
+            id
+            text
+          }
+        }
+      }
+    `,
+  })
+  .then(result => console.log(result));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
