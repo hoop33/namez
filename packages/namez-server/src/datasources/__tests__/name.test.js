18017abe08ba723a093a1ba74535991a77af6c35
Rob Warner <rob.warner@availity.com>
2019-02-14 17:59:58 -0500

    Refactor and add tests

--- packages/namez-server/src/datasources/__tests__/name.test.js
+++ packages/namez-server/src/datasources/__tests__/name.test.js
@@ -1,57 +1,105 @@
 const NameAPI = require('../name');
+const RollingStore = require('../rolling_store');
+const WordnikAPI = require('../wordnik');
 
-test('nameReducer should not return null', () => {
-  expect(NameAPI.nameReducer()).toBeTruthy();
+describe('[NameAPI.nameReducer]', () => {
+  it('should not return null', () => {
+    expect(NameAPI.nameReducer()).toBeTruthy();
+  });
+
+  it('should generate an ID', () => {
+    expect(NameAPI.nameReducer().id).toBeTruthy();
+  });
+
+  it('should create text from adj and noun', () => {
+    expect(NameAPI.nameReducer('Adj', 'Noun').text).toBe('Adj Noun');
+  });
+
+  it('should create blank text when noun not present', () => {
+    expect(NameAPI.nameReducer('Adj').text).toBe('');
+  });
+
+  it('should create blank text when noun is null', () => {
+    expect(NameAPI.nameReducer('Adj', null).text).toBe('');
+  });
+
+  it('should create blank text when noun is undefined', () => {
+    expect(NameAPI.nameReducer('Adj', undefined).text).toBe('');
+  });
+
+  it('should create blank text when noun is blank', () => {
+    expect(NameAPI.nameReducer('Adj', '').text).toBe('');
+  });
+
+  it('should create blank text when adj is null', () => {
+    expect(NameAPI.nameReducer(null, 'Noun').text).toBe('');
+  });
+
+  it('should create blank text when noun is undefined', () => {
+    expect(NameAPI.nameReducer(undefined, 'Noun').text).toBe('');
+  });
+
+  it('should create blank text when noun is blank', () => {
+    expect(NameAPI.nameReducer('', 'Noun').text).toBe('');
+  });
+
+  it('should create title case when all lowercase', () => {
+    expect(NameAPI.nameReducer('adj', 'noun').text).toBe('Adj Noun');
+  });
+
+  it('should create title case when all uppercase', () => {
+    expect(NameAPI.nameReducer('ADJ', 'NOUN').text).toBe('Adj Noun');
+  });
+
+  it('should create title case when all title case', () => {
+    expect(NameAPI.nameReducer('Adj', 'Noun').text).toBe('Adj Noun');
+  });
+
+  it('should handle 1-length strings', () => {
+    expect(NameAPI.nameReducer('a', 'n').text).toBe('A N');
+  });
 });
 
-test('nameReducer should generate an ID', () => {
-  expect(NameAPI.nameReducer().id).toBeTruthy();
+describe('[NameAPI.getNames]', () => {
+  it('should return empty when limit is 0', async () => {
+    const names = new NameAPI(new RollingStore());
+    const res = await names.getNames('', 0, null);
+    expect(res).toEqual([]);
+  });
+
+  it('should return one item when limit is 1', async () => {
+    const names = new NameAPI(new RollingStore());
+    const wordnik = new WordnikAPI();
+    wordnik.getRandomWords = jest
+    .fn()
+    .mockReturnValueOnce(mockWordnikAdjectiveResponse)
+    .mockReturnValueOnce(mockWordnikNounResponse);
+
+    const res = await names.getNames('', 1, wordnik);
+    expect(res.length).toBe(1);
+    expect(res[0].id).toBeTruthy();
+    expect(res[0].text).toBe('First Apple');
+  });
+
+  it('should return five items when limit is 5', async () => {
+    const names = new NameAPI(new RollingStore());
+    const wordnik = new WordnikAPI();
+    wordnik.getRandomWords = jest
+    .fn()
+    .mockReturnValueOnce(mockWordnikAdjectiveResponse)
+    .mockReturnValueOnce(mockWordnikNounResponse);
+
+    const res = await names.getNames('', 5, wordnik);
+    expect(res.length).toBe(5);
+  });
 });
 
-test('nameReducer should create text from adj and noun', () => {
-  expect(NameAPI.nameReducer('Adj', 'Noun').text).toBe('Adj Noun');
-});
+// Mock data
 
-test('nameReducer should create blank text when noun not present', () => {
-  expect(NameAPI.nameReducer('Adj').text).toBe('');
-});
+const mockWordnikAdjectiveResponse = [
+  'first', 'second', 'third', 'fourth', 'fifth'
+];
 
-test('nameReducer should create blank text when noun is null', () => {
-  expect(NameAPI.nameReducer('Adj', null).text).toBe('');
-});
-
-test('nameReducer should create blank text when noun is undefined', () => {
-  expect(NameAPI.nameReducer('Adj', undefined).text).toBe('');
-});
-
-test('nameReducer should create blank text when noun is blank', () => {
-  expect(NameAPI.nameReducer('Adj', '').text).toBe('');
-});
-
-test('nameReducer should create blank text when adj is null', () => {
-  expect(NameAPI.nameReducer(null, 'Noun').text).toBe('');
-});
-
-test('nameReducer should create blank text when noun is undefined', () => {
-  expect(NameAPI.nameReducer(undefined, 'Noun').text).toBe('');
-});
-
-test('nameReducer should create blank text when noun is blank', () => {
-  expect(NameAPI.nameReducer('', 'Noun').text).toBe('');
-});
-
-test('nameReducer should create title case when all lowercase', () => {
-  expect(NameAPI.nameReducer('adj', 'noun').text).toBe('Adj Noun');
-});
-
-test('nameReducer should create title case when all uppercase', () => {
-  expect(NameAPI.nameReducer('ADJ', 'NOUN').text).toBe('Adj Noun');
-});
-
-test('nameReducer should create title case when all title case', () => {
-  expect(NameAPI.nameReducer('Adj', 'Noun').text).toBe('Adj Noun');
-});
-
-test('nameReducer should handle 1-length strings', () => {
-  expect(NameAPI.nameReducer('a', 'n').text).toBe('A N');
-});
+const mockWordnikNounResponse = [
+  'apple', 'baker', 'charlie', 'delta', 'eggplant'
+];
--- packages/namez-server/src/datasources/__tests__/rolling_store.test.js
+++ packages/namez-server/src/datasources/__tests__/rolling_store.test.js
@@ -1,182 +1,202 @@
 const RollingStore = require('../rolling_store');
 
-test('addItem should not add item when null', () => {
-  const store = new RollingStore();
-  store.addItem(null);
-  expect(store.size()).toBe(0);
+describe('[RollingStore.addItem]', () => {
+  it('should not add item when null', () => {
+    const store = new RollingStore();
+    store.addItem(null);
+    expect(store.size()).toBe(0);
+  });
+
+  it('should not add item when undefined', () => {
+    const store = new RollingStore();
+    store.addItem(undefined);
+    expect(store.size()).toBe(0);
+  });
+
+  it('should not add item when no id', () => {
+    const store = new RollingStore();
+    store.addItem({});
+    expect(store.size()).toBe(0);
+  });
+
+  it('should not add item when id is null', () => {
+    const store = new RollingStore();
+    store.addItem({id: null});
+    expect(store.size()).toBe(0);
+  });
+
+  it('should not add item when id is undefined', () => {
+    const store = new RollingStore();
+    store.addItem({id: undefined});
+    expect(store.size()).toBe(0);
+  });
+
+  it('should not add item when id is empty', () => {
+    const store = new RollingStore();
+    store.addItem({id: ''});
+    expect(store.size()).toBe(0);
+  });
+
+  it('should add item when has id', () => {
+    const store = new RollingStore();
+    store.addItem({id: '1'});
+    expect(store.size()).toBe(1);
+  });
+
+  it('should replace same id', () => {
+    const store = new RollingStore();
+    store.addItem({id: '1'});
+    store.addItem({id: '1'});
+    expect(store.size()).toBe(1);
+  });
+
+  it('should not replace different id', () => {
+    const store = new RollingStore();
+    store.addItem({id: '1'});
+    store.addItem({id: '2'});
+    expect(store.size()).toBe(2);
+  });
 });
 
-test('addItem should not add item when undefined', () => {
-  const store = new RollingStore();
-  store.addItem(undefined);
-  expect(store.size()).toBe(0);
+describe('[RollingStore.size]', () => {
+  it('should return correct size', () => {
+    const store = new RollingStore(10);
+    store.addItem({id: '1'});
+    store.addItem({id: '2'});
+    store.addItem({id: '3'});
+    store.addItem({id: '4'});
+    store.addItem({id: '5'});
+    expect(store.size()).toBe(5);
+  });
+
+  it('should not exceed max size', () => {
+    const store = new RollingStore(1);
+    store.addItem({id: '1'});
+    store.addItem({id: '2'});
+    expect(store.size()).toBe(1);
+  });
 });
 
-test('addItem should not add item when no id', () => {
-  const store = new RollingStore();
-  store.addItem({});
-  expect(store.size()).toBe(0);
+describe('[RollingStore.findItem]', () => {
+  it('should return null when id is null', () => {
+    const store = new RollingStore();
+    expect(store.findItem(null)).toBeNull();
+  });
+
+  it('should return null when id is undefined', () => {
+    const store = new RollingStore();
+    expect(store.findItem(undefined)).toBeNull();
+  });
+
+  it('should return null when id is empty', () => {
+    const store = new RollingStore();
+    expect(store.findItem('')).toBeNull();
+  });
+
+  it('should return null when not found', () => {
+    const store = new RollingStore();
+    expect(store.findItem('1')).toBeNull();
+  });
+
+  it('should find item when present', () => {
+    const store = new RollingStore();
+    store.addItem({id: '1'});
+    store.addItem({id: '2'});
+    store.addItem({id: '3'});
+    store.addItem({id: '4'});
+    expect(store.findItem('3').id).toBe('3');
+  });
 });
 
-test('addItem should not add item when id is null', () => {
-  const store = new RollingStore();
-  store.addItem({id: null});
-  expect(store.size()).toBe(0);
+describe('[RollingStore]', () => {
+  it('should handle 500 additions and roll items', () => {
+    const store = new RollingStore(250);
+    for (let i = 0; i < 500; i++) {
+      store.addItem({id: i.toString()});
+    }
+    expect(store.findItem('0')).toBeNull();
+    expect(store.findItem('249')).toBeNull();
+    expect(store.findItem('250').id).toBe('250');
+    expect(store.findItem('499').id).toBe('499');
+  });
+
+  it('should handle 500 additions and cap at max size', () => {
+    const store = new RollingStore(250);
+    for (let i = 0; i < 500; i++) {
+      store.addItem({id: i.toString()});
+    }
+    expect(store.size()).toBe(250);
+  });
 });
 
-test('addItem should not add item when id is undefined', () => {
-  const store = new RollingStore();
-  store.addItem({id: undefined});
-  expect(store.size()).toBe(0);
-});
+describe('[RollingStore.findItems]', () => {
+  it('should return empty when store is empty', () => {
+    const store = new RollingStore();
+    expect(store.findItems()).toEqual([]);
+  });
 
-test('addItem should not add item when id is empty', () => {
-  const store = new RollingStore();
-  store.addItem({id: ''});
-  expect(store.size()).toBe(0);
-});
+  it('should return empty when num is less than 1', () => {
+    const store = new RollingStore();
+    store.addItem({id: '1'});
+    store.addItem({id: '2'});
+    store.addItem({id: '3'});
+    const items = store.findItems('1', 0);
+    expect(store.findItems()).toEqual([]);
+  });
 
-test('addItem should add item when has id', () => {
-  const store = new RollingStore();
-  store.addItem({id: '1'});
-  expect(store.size()).toBe(1);
-});
+  it('should return empty when size is less than num', () => {
+    const store = new RollingStore();
+    store.addItem({id: '1'});
+    store.addItem({id: '2'});
+    store.addItem({id: '3'});
+    const items = store.findItems('1', 4);
+    expect(store.findItems()).toEqual([]);
+  });
 
-test('addItem should replace same id', () => {
-  const store = new RollingStore();
-  store.addItem({id: '1'});
-  store.addItem({id: '1'});
-  expect(store.size()).toBe(1);
-});
+  it('should return first item when after is null', () => {
+    const store = new RollingStore();
+    store.addItem({id: '1'});
+    store.addItem({id: '2'});
+    store.addItem({id: '3'});
+    const items = store.findItems(null, 3);
+    expect(items.length).toBe(3);
+    expect(items[0].id).toBe('1');
+  });
 
-test('addItem should not replace different id', () => {
-  const store = new RollingStore();
-  store.addItem({id: '1'});
-  store.addItem({id: '2'});
-  expect(store.size()).toBe(2);
-});
+  it('should return first item when after is undefined', () => {
+    const store = new RollingStore();
+    store.addItem({id: '1'});
+    store.addItem({id: '2'});
+    store.addItem({id: '3'});
+    const items = store.findItems(undefined, 3);
+    expect(items.length).toBe(3);
+    expect(items[0].id).toBe('1');
+  });
 
-test('size should not exceed max size', () => {
-  const store = new RollingStore(1);
-  store.addItem({id: '1'});
-  store.addItem({id: '2'});
-  expect(store.size()).toBe(1);
-});
+  it('should return first item when after is blank', () => {
+    const store = new RollingStore();
+    store.addItem({id: '1'});
+    store.addItem({id: '2'});
+    store.addItem({id: '3'});
+    const items = store.findItems('', 3);
+    expect(items.length).toBe(3);
+    expect(items[0].id).toBe('1');
+  });
 
-test('findItem should return null when id is null', () => {
-  const store = new RollingStore();
-  expect(store.findItem(null)).toBeNull();
-});
-
-test('findItem should return null when id is undefined', () => {
-  const store = new RollingStore();
-  expect(store.findItem(undefined)).toBeNull();
-});
-
-test('findItem should return null when id is empty', () => {
-  const store = new RollingStore();
-  expect(store.findItem('')).toBeNull();
-});
-
-test('findItem should return null when not found', () => {
-  const store = new RollingStore();
-  expect(store.findItem('1')).toBeNull();
-});
-
-test('findItem should find item when present', () => {
-  const store = new RollingStore();
-  store.addItem({id: '1'});
-  store.addItem({id: '2'});
-  store.addItem({id: '3'});
-  store.addItem({id: '4'});
-  expect(store.findItem('3').id).toBe('3');
-});
-
-test('store should handle 500 additions and roll items', () => {
-  const store = new RollingStore(250);
-  for (let i = 0; i < 500; i++) {
-    store.addItem({id: i.toString()});
-  }
-  expect(store.findItem('0')).toBeNull();
-  expect(store.findItem('249')).toBeNull();
-  expect(store.findItem('250').id).toBe('250');
-  expect(store.findItem('499').id).toBe('499');
-});
-
-test('store should handle 500 additions and cap at max size', () => {
-  const store = new RollingStore(250);
-  for (let i = 0; i < 500; i++) {
-    store.addItem({id: i.toString()});
-  }
-  expect(store.size()).toBe(250);
-});
-
-test('findItems should return empty when store is empty', () => {
-  const store = new RollingStore();
-  expect(store.findItems()).toEqual([]);
-});
-
-test('findItems should return empty when num is less than 1', () => {
-  const store = new RollingStore();
-  store.addItem({id: '1'});
-  store.addItem({id: '2'});
-  store.addItem({id: '3'});
-  const items = store.findItems('1', 0);
-  expect(store.findItems()).toEqual([]);
-});
-
-test('findItems should return empty when size is less than num', () => {
-  const store = new RollingStore();
-  store.addItem({id: '1'});
-  store.addItem({id: '2'});
-  store.addItem({id: '3'});
-  const items = store.findItems('1', 4);
-  expect(store.findItems()).toEqual([]);
-});
-
-test('findItems should return first item when after is null', () => {
-  const store = new RollingStore();
-  store.addItem({id: '1'});
-  store.addItem({id: '2'});
-  store.addItem({id: '3'});
-  const items = store.findItems(null, 3);
-  expect(items.length).toBe(3);
-  expect(items[0].id).toBe('1');
-});
-
-test('findItems should return first item when after is undefined', () => {
-  const store = new RollingStore();
-  store.addItem({id: '1'});
-  store.addItem({id: '2'});
-  store.addItem({id: '3'});
-  const items = store.findItems(undefined, 3);
-  expect(items.length).toBe(3);
-  expect(items[0].id).toBe('1');
-});
-
-test('findItems should return first item when after is blank', () => {
-  const store = new RollingStore();
-  store.addItem({id: '1'});
-  store.addItem({id: '2'});
-  store.addItem({id: '3'});
-  const items = store.findItems('', 3);
-  expect(items.length).toBe(3);
-  expect(items[0].id).toBe('1');
-});
-
-test('findItems should find item after id specified and return items', () => {
-  const store = new RollingStore();
-  store.addItem({id: '1'});
-  store.addItem({id: '2'});
-  store.addItem({id: '3'});
-  store.addItem({id: '4'});
-  store.addItem({id: '5'});
-  store.addItem({id: '6'});
-  store.addItem({id: '7'});
-  store.addItem({id: '8'});
-  const items = store.findItems('4', 3);
-  expect(items.length).toBe(3);
-  expect(items[0].id).toBe('5');
-  expect(items[1].id).toBe('6');
-  expect(items[2].id).toBe('7');
+  it('should find item after id specified and return items', () => {
+    const store = new RollingStore();
+    store.addItem({id: '1'});
+    store.addItem({id: '2'});
+    store.addItem({id: '3'});
+    store.addItem({id: '4'});
+    store.addItem({id: '5'});
+    store.addItem({id: '6'});
+    store.addItem({id: '7'});
+    store.addItem({id: '8'});
+    const items = store.findItems('4', 3);
+    expect(items.length).toBe(3);
+    expect(items[0].id).toBe('5');
+    expect(items[1].id).toBe('6');
+    expect(items[2].id).toBe('7');
+  });
 });
