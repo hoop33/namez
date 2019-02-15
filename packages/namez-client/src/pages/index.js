119db0860f8147b03cc4c7b5a9548c828eb25088
Rob Warner <rob.warner@availity.com>
2019-02-15 11:40:29 -0500

    Make page background dark

--- packages/namez-client/src/pages/index.js
+++ packages/namez-client/src/pages/index.js
@@ -1,11 +1,18 @@
 import React, {Fragment} from 'react';
-
+import {createGlobalStyle} from 'styled-components';
 import Names from './names';
 import {Footer, Header, PageContainer} from '../components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #363636;
+  }
+`;
 
 export default function Pages() {
   return (
     <Fragment>
+      <GlobalStyle />
       <Header />
       <PageContainer>
         <Names path="/" />
