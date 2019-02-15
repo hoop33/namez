567299d49eb10e2da129a3d1f88168c2d4484cf3
Rob Warner <rob.warner@availity.com>
2019-02-15 13:27:22 -0500

    Remove unused fragment

--- packages/namez-client/src/pages/names.js
+++ packages/namez-client/src/pages/names.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import {Query} from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
@@ -27,25 +27,21 @@ const NamesContainer = styled.div`
 export default function Names() {
   return (
     <Query query={GET_NAMES}>
-      {({data, loading, error}) => {
-        if (error) return <p>ERROR</p>;
+      {({data, loading, error, fetchMore}) => {
+        if (error) return <p>ERROR: ${error}</p>;
 
         return (
-          <Fragment>
-            <NamesContainer>
-              {data.names &&
-                data.names.nodes &&
-                data.names.nodes.map(name => {
-                  return (
-                    <NameTile
-                      key={name.id}
-                      name={name}
-                      color={colors.nextColor()}
-                    />
-                  );
-                })}
-            </NamesContainer>
-          </Fragment>
+          <NamesContainer>
+            {data.names &&
+              data.names.nodes &&
+              data.names.nodes.map(name => (
+                <NameTile
+                  key={name.id}
+                  name={name}
+                  color={colors.nextColor()}
+                />
+              ))}
+          </NamesContainer>
         );
       }}
     </Query>
